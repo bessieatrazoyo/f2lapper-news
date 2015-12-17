20e60a288c1136e7f9ea4cdcf0efa970c88aba4d
bessieatrazoyo <bmalek@razoyo.com>
2015-12-17 16:23:02 -0600

    Added login

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router'])
+var app = angular.module ('flapperNews', ['ui.router'])
 .config ([
   '$stateProvider',
   '$urlRouterProvider',
@@ -10,27 +10,135 @@ var app = angular.module('flapperNews', ['ui.router'])
         controller: 'MainCtrl',
         resolve: {
           postPromise: ['posts', function (posts) {
-            return posts.getAll();
+            return posts.getAll ();
           }]
         } 
       })
 
-      .state('posts', {
+      .state ('posts', {
         url: '/posts/{id}',
         templateUrl: '/posts.html',
         controller: 'PostsCtrl',
         resolve: {
           post: ['$stateParams', 'posts', function ($stateParams, posts) {
-            return posts.get ($stateParams.id);
+            return posts.get  ($stateParams.id);
           }]
         }
+      })
+
+      .state ('login', {
+        url: '/login',
+        templateUrl: '/login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function ($state, auth) {
+          if (auth.isLoggedIn ()) {
+            $state.go ('home');
+          }
+        }]
+      })
+
+      .state ('register', {
+        url: '/register',
+        templateUrl: '/register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function ($state, auth) {
+          if (auth.isLoggedIn ()) {
+            $state.go ('home');
+          }
+        }]
       });
 
-      $urlRouterProvider.otherwise('home');
+      $urlRouterProvider.otherwise ('home');
     }
 ])
 
-.factory ('posts', ['$http', function ($http) {
+.factory ('auth', ['$http', '$window', function ($http, $window) {
+  var auth = {};
+
+  auth.saveToken = function (token) {
+    $window.localStorage['flapper-news-token'] = token;
+  }
+
+  auth.getToken = function () {
+    return $window.localStorage['flapper-news-token'];
+  }
+
+  auth.isLoggedIn = function () {
+    var token = auth.getToken();
+
+    if (token) {
+      var payload = JSON.parse ($window.atob (token.split ('.')[1]));
+
+      return payload.exp > Date.now () / 1000;
+    } else {
+      return false;
+    }
+  }
+
+  auth.currentUser = function () {
+    if (auth.isLoggedIn ()) {
+      var token = auth.getToken ();
+      var payload = JSON.parse ($window.atob (token.split ('.')[1]));
+
+      return payload.username;
+    }
+  }
+
+  auth.register = function (user) {
+    return $http.post ('/register', user).success (function (data) {
+      auth.saveToken (data.token);
+    });
+  }
+  
+  auth.logIn = function (user) {
+    return $http.post ('/login', user).success (function (data) {
+      auth.saveToken (data.token);
+    });
+  };
+  
+  auth.logOut = function () {
+    $window.localStorage.removeItem ('flapper-news-token');
+  };
+
+
+
+  return auth;
+}])
+
+.controller ('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function ($scope, $state, auth) {
+  $scope.user = {};
+
+  $scope.register = function () {
+    auth.register ($scope.user).error (function (error) {
+      $scope.error = error;
+    }).then (function () {
+      $state.go ('home');
+    });
+  };
+
+  $scope.logIn = function () {
+    auth.logIn ($scope.user).error (function (error) {
+      $scope.error = error;
+    }).then (function () {
+      $state.go ('home');
+    });
+  };
+}])
+
+.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}])
+
+.factory  ('posts', ['$http', function ($http) {
   var o = {
     posts: []
   };
@@ -65,10 +173,10 @@ var app = angular.module('flapperNews', ['ui.router'])
   return o;
 }])
 
-.controller('MainCtrl', [
+.controller ('MainCtrl', [
   '$scope',
   'posts',
-  function($scope, posts){
+  function ($scope, posts){
     $scope.posts = posts.posts; 
 
     $scope.addPost = function () {
